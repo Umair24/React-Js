2354960c9dd7f7ec1d12e9988fbadaf118d01096
Umair Ansari <ansariumair2402@gmail.com>
2024-02-17 14:13:06 +0530

    Counter Project

---
+++ 02_Counter/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState,  } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 10
+
+  let addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }
+  }
+
+  let removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  let resetValue = () => {
+    setCounter(0)
+  }
+
+  return (
+    <>
+    <h1>React Course</h1>
+    <h2>Counter Value: {counter}</h2>
+
+    <button 
+    onClick={addValue}
+    >Add Value</button>
+
+    <button
+    onClick={removeValue}
+    >Remove Value </button>
+
+    <p>Footer: {counter}</p>
+
+    <button
+    onClick={resetValue}
+    >Reset</button>
+    </>
+  )
+}
+
+export default App

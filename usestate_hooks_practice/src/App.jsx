09179e21e1d7cc753e5bc27f1a88227ff9848ca0
Umair Ansari <ansariumair2402@gmail.com>
2024-03-02 11:26:43 +0530

    hooks practice

--- usestate_hooks_practice/src/App.jsx
+++ usestate_hooks_practice/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -54,3 +54,7 @@ function App() {
 }
 
 export default App
+
+
+
+
